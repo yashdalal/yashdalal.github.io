d23bc3988bd560690bccfa33b903254f7b271f7c
Yash Dalal <yashdalal95@gmail.com>
2015-09-18 13:09:21 -0500

    modified index, js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+function parse(){
+	var x = document.getElementById("text");
+	document.getElementById("demo").innerHTML= x;
+}
+
+
 $(document).ready(function() {
 	$("#text").on("input", function(){
 		var text = $("text").val();
