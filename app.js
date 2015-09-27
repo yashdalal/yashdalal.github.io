883b10b61bc48110832a4787f01ccf9b342627c2
Yash Dalal <yashdalal95@gmail.com>
2015-09-26 19:06:57 -0500

    api request

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
+function apiCall() {
+	var apiKeyMedDict = "aeee5df0-8558-4f8d-8d5d-83d30f069852";
+	var query = document.getElementById("search");
+	var url = "http://www.dictionaryapi.com/api/v1/references/medical/xml/"+ query.value +"?key="+apiKeyMedDict;
+	var xhr = createCORSRequest('GET', url);
+	xhr.setRequestHeader('Content-Type', 'text/xml');
+	xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+	if (!xhr) {
+    	alert('CORS not supported');
+    	return;
+  	}
+	//xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+	xhr.onload = function() {
+	    var text = xhr.responseText;
+	    var title = getTitle(text);
+	    console.log(text);
+	    alert('Response from CORS request to ' + url + ': ' + title);
+	};
+
+	xhr.onerror = function() {
+		alert('Woops, there was an error making the request.');
+	};
+
+	xhr.send();
+}
+
+function createCORSRequest(method, url) {
+	var xhr = new XMLHttpRequest();
+	if ("withCredentials" in xhr) {
+		xhr.open(method, url, true);
+	} else {
+		xhr = null;
+	}
+	return xhr;
+}
+
+function getTitle(text) {
+  return text.match('<title>(.*)?</title>')[1];
+}
+
+
+
 function parse(){
 	var x = document.getElementById("text");//.value.split(" ");
 	//var output;
